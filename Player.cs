2f4d3897528e1c7cbb456b90792ba2c3267fb22a
César Zárate <cesarjzo@outlook.com>
2023-09-17 17:29:15 -0600

    Add Hit signal and OnBodyEntered function

--- Player.cs
+++ Player.cs
@@ -5,12 +5,27 @@ namespace DodgeTheCreeps;
 
 public sealed partial class Player : Area2D
 {
+    [Signal]
+    public delegate void HitEventHandler();
+
+    [Export] private bool startHidden;
     [Export] private int speed = 400;
     private Vector2 _screenSize;
+
+    public void OnBodyEntered(PhysicsBody2D body)
+    {
+        // Player disappears after being hit.
+        Hide();
+        EmitSignal(SignalName.Hit);
+        // Must be deferred as we can't change physics properties on a physics callback.
+        GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
+    }
 
     public override void _Ready()
     {
         _screenSize = GetViewportRect().Size;
+        if (startHidden)
+            Hide();
     }
 
     public override void _Process(double delta)
@@ -43,5 +58,17 @@ public sealed partial class Player : Area2D
             x: Mathf.Clamp(Position.X, 0, _screenSize.X),
             y: Mathf.Clamp(Position.Y, 0, _screenSize.Y)
         );
+
+        if (velocity.X != 0f)
+        {
+            animatedSprite.Animation = "walk";
+            animatedSprite.FlipV = velocity.Y > 0f;
+            animatedSprite.FlipH = velocity.X < 0f;
+        }
+        else if (velocity.Y != 0f)
+        {
+            animatedSprite.Animation = "up";
+            animatedSprite.FlipV = velocity.Y > 0f;
+        }
     }
 }
